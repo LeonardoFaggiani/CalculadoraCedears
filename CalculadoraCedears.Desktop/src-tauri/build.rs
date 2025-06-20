7c4446ce9e762b1bf6a7f9bb4a44007aae5e4c93
Leonardo Faggiani <leonardofaggiani@hotmail.com>
2025-06-20 18:49:47 -0300

    Se crea action para deployar la API por separado

--- CalculadoraCedears.Desktop/src-tauri/build.rs
+++ CalculadoraCedears.Desktop/src-tauri/build.rs
@@ -1,11 +1,8 @@
 fn main() {
     tauri_build::build();
 
-      // Embedear la variable API_BASE_URL en el binario
-    if let Ok(api_base_url) = std::env::var("API_BASE_URL") {
-        println!("cargo:rustc-env=API_BASE_URL={}", api_base_url);
-    } else {
-        // Esto permite tener un valor por defecto si no se define
-        println!("cargo:rustc-env=API_BASE_URL=https://localhost:7016/api/");
-    }
+    let api_base_url = std::env::var("API_BASE_URL")
+        .unwrap_or("https://localhost:7016/api/".to_string());
+
+    println!("cargo:rustc-env=API_BASE_URL={}", api_base_url);
 }
