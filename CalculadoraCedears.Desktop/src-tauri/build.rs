7d96ee204833d1c09d4cd0ad98d1916b8590a1e4
Leonardo Faggiani <leonardofaggiani@hotmail.com>
2025-06-20 08:23:26 -0300

    Cambio lectura de variable api

--- CalculadoraCedears.Desktop/src-tauri/build.rs
+++ CalculadoraCedears.Desktop/src-tauri/build.rs
@@ -1,3 +1,11 @@
 fn main() {
-    tauri_build::build()
+    tauri_build::build();
+
+      // Embedear la variable API_BASE_URL en el binario
+    if let Ok(api_base_url) = std::env::var("API_BASE_URL") {
+        println!("cargo:rustc-env=API_BASE_URL={}", api_base_url);
+    } else {
+        // Esto permite tener un valor por defecto si no se define
+        println!("cargo:rustc-env=API_BASE_URL=https://localhost:7016/api/");
+    }
 }
