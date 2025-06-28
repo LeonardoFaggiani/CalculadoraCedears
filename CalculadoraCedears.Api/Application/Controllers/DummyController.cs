c75a5e7c0a56b3f3b61992ab4e755cfc7fc72d9c
Leonardo Faggiani <leonardofaggiani@hotmail.com>
2025-06-28 13:14:06 -0300

    Se agrega dummy controller para testeo

---
+++ CalculadoraCedears.Api/Application/Controllers/DummyController.cs
@@ -0,0 +1,35 @@
+﻿using CalculadoraCedears.Api.Application.Controllers.Base;
+
+using CommunityToolkit.Diagnostics;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraCedears.Api.Application.Controllers
+{
+    public class DummyController : ApiController
+    {
+        private readonly IConfiguration configuration;
+        public DummyController(IMediator mediator, IConfiguration configuration) : base(mediator)
+        {
+            Guard.IsNotNull(configuration, nameof(configuration));
+
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetAsync(CancellationToken cancellationToken)
+        {
+
+            var values = new Dictionary<string, string>
+            {
+                { "redirect_uri", this.configuration["GoogleClient:RedirectUri"] },
+            };
+
+            return Ok(values);
+        }
+    }
+}
